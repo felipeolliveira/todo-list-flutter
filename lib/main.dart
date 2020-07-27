0afa57f88eddb59dd48d5e7b2c5010fd7731d281
Felipe Oliveira <flpoliveira42@gmail.com>
2020-07-27 15:12:07 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,134 @@
+import 'dart:convert';
+import 'package:flutter/material.dart'; //material Android
+import 'package:flutter/cupertino.dart';
+import 'package:TodoList/models/item.dart'; //material IOS
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //sempre o material app primeiro, e em seguida o theme o theme e home
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  //lista de itens que vai pegar do json
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    if (newTaskCtrl.text.isEmpty) return;
+    setState(() {
+      widget.items.add(
+        Item(
+          title: newTaskCtrl.text,
+          done: false,
+        ),
+      );
+      newTaskCtrl.text = "";
+      save();
+    });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState() {
+    load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //scaffold representa uma pagina (usar sempre que tem pagina)
+      appBar: AppBar(
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          decoration: InputDecoration(
+            labelText: "Toque aqui para adicionar uma nova tarefa",
+            labelStyle: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        //user o ListView.builder (builder atualiza conforme a criacao de novos items)
+        itemCount: widget.items.length, //pega o tamanho do array de items
+        itemBuilder: (BuildContext ctxt, int index) {
+          //o que vai desenhar ()
+          final item = widget.items[index];
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                  save();
+                });
+              },
+            ),
+            key: Key(item.title),
+            background: Container(
+              color: Colors.red.withOpacity(0.4),
+            ),
+            onDismissed: (direction) {
+              remove(index);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue[300],
+      ),
+    );
+  }
+}
